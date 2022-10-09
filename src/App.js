841b85c37956d99c676bcc90f61b5dc0c89aebf2
Tia Bishop <tia.bishop25@gmail.com>
2022-10-09 01:52:14 -0500

    Adding files for front end

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { useState } from 'react';
+import Button from './Button';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <heading>Become an AAdvantage Member Today</heading>
+        <Button/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React, {Component} from 'react';
+import { useState } from 'react'
+
+
+function Button() {
+  return (
+    <div className="App">
+      <button>Make Me a Member!</button>
+    </div>
+  );
+}
+
+export default Button;
